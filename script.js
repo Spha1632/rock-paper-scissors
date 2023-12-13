12f8a5fa996dffe4b207409bcbe1e794326b0c0e
Alson <sphamandlan1632@gmail.com>
2023-12-13 15:17:04 +0200

    Created image folder and style.css file

--- script.js
+++ script.js
@@ -1,41 +1,73 @@
-function game() {
-
-    function getComputerChoice() {
-        const RPS = ["rock", "paper", "scissors"];
-
-        return RPS[Math.floor(Math.random() * 3)];
-    }
-
-    function getPlayerChoice() {
-        return prompt("Enter rock, paper or scissors").toLowerCase();
-    }
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            alert(`Tie! You chose ${playerSelection} and Computer chose ${computerSelection}`);
-            return `Tie! You chose ${playerSelection} and Computer chose ${computerSelection}`;
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
-            return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
-            return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
-            return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
-        } else {
-            alert(`You lose! You chose ${playerSelection} and Computer chose ${computerSelection}`);
-            return `You lose!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
-        }
-    }
 
 
-       
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+function getComputerChoice() {
+    const RPS = ["rock", "paper", "scissors"];
+
+    return RPS[Math.floor(Math.random() * 3)];
 }
 
-const rock = document.querySelector("rock");
-const paper = document.querySelector("paper");
-const scissors = document.querySelector("scissors");
+/* function getPlayerChoice() {
+    return prompt("Enter rock, paper or scissors").toLowerCase();
+} */
+
+let playerScore = 0;
+let ComputerScore = 0;
+
+let message;
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        //alert(`Tie! You chose ${playerSelection} and Computer chose ${computerSelection}`);
+        return `Tie! You chose ${playerSelection} and Computer chose ${computerSelection}`;
+
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        //alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
+        playerScore++;
+        `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+        
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        //alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
+        playerScore++;
+        return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        //alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
+        playerScore++;
+        return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+
+    } else {
+        //alert(`You lose! You chose ${playerSelection} and Computer chose ${computerSelection}`);
+        ComputerScore++;
+        return `You lose!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+        
+    }
+}
+
+
+    
+//const playerSelection = getPlayerChoice();
+let playerSelection;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results = document.querySelector("#results")
+const score = document.querySelector("#score")
+
+
+
+rock.addEventListener("click", function () {
+    results.textContent = playRound("rock", computerSelection);
+});
+paper.addEventListener("click", function () {
+    results.textContent = playRound("paper", computerSelection);
+});
+scissors.addEventListener("click", function () {
+    results.textContent = playRound("scissors", computerSelection);
+});
+
+score.textContent = playerScore;
