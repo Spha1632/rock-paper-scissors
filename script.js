d54278db9ffa9e966581f349a6ecfff14b14ad5b
Alson <sphamandlan1632@gmail.com>
2023-12-15 10:15:48 +0200

    Added all logic needed for game to work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const RPS = ["rock", "paper", "scissors"];
@@ -6,68 +7,75 @@ function getComputerChoice() {
     return RPS[Math.floor(Math.random() * 3)];
 }
 
-/* function getPlayerChoice() {
-    return prompt("Enter rock, paper or scissors").toLowerCase();
-} */
-
-let playerScore = 0;
-let ComputerScore = 0;
-
-let message;
-
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        //alert(`Tie! You chose ${playerSelection} and Computer chose ${computerSelection}`);
         return `Tie! You chose ${playerSelection} and Computer chose ${computerSelection}`;
 
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        //alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
         playerScore++;
-        `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+        return `You win! You chose ${playerSelection} and Computer chose ${computerSelection}`;
         
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        //alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
         playerScore++;
-        return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+        return `You win! You chose ${playerSelection} and Computer chose ${computerSelection}`;
 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        //alert(`You win! You chose ${playerSelection} and Computer chose ${computerSelection}`);
         playerScore++;
-        return `You win!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+        return `You win! You chose ${playerSelection} and Computer chose ${computerSelection}`;
 
     } else {
-        //alert(`You lose! You chose ${playerSelection} and Computer chose ${computerSelection}`);
-        ComputerScore++;
-        return `You lose!  You chose ${playerSelection} and Computer chose ${computerSelection}`;
+        computerScore++;
+        return `You lose! You chose ${playerSelection} and Computer chose ${computerSelection}`;
         
     }
 }
 
 
-    
-//const playerSelection = getPlayerChoice();
-let playerSelection;
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
-const results = document.querySelector("#results")
-const score = document.querySelector("#score")
-
+const results = document.querySelector("#results");
+const playerScoreResults = document.querySelector("#playerScore");
+const computerScoreResults = document.querySelector("#computerScore");
 
 
 rock.addEventListener("click", function () {
+    const computerSelection = getComputerChoice();
     results.textContent = playRound("rock", computerSelection);
+    updateScore();
+    checkWinner();
 });
 paper.addEventListener("click", function () {
+    const computerSelection = getComputerChoice();
     results.textContent = playRound("paper", computerSelection);
+    updateScore();
+    checkWinner();
 });
 scissors.addEventListener("click", function () {
+    const computerSelection = getComputerChoice();
     results.textContent = playRound("scissors", computerSelection);
+    updateScore();
+    checkWinner();
 });
 
-score.textContent = playerScore;
+function updateScore() {
+    playerScoreResults.textContent = playerScore;
+    computerScoreResults.textContent = computerScore;
+}
+
+function checkWinner() {
+    if (playerScore === 5) {
+      results.textContent = "Congratulations! You won the game!";
+      resetGame();
+    } else if (computerScore === 5) {
+      results.textContent = "Sorry! Computer won the game.";
+      resetGame();
+    }
+}
+
+function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
+}
+
